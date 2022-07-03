5393551abbd1e7e48641dfde82c50119eccb5636
Ivan Gavrilich <81584193+joseffie@users.noreply.github.com>
2022-07-03 22:13:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-const links = document.querySelectorAll('a[data-goto]');
-if (links.length > 0) {
-	links.forEach(link => {
-		link.addEventListener('click', scrollingOnClick);
+const scrollLinks = document.querySelectorAll('a[data-scroll-link]');
+if (scrollLinks.length > 0) {
+  scrollLinks.forEach((scrollLink) => {
+    scrollLink.addEventListener('click', scrollingOnClick);
   });
 
-	function scrollingOnClick(e) {
-		const link = e.target;
-		if (link.dataset.goto && document.querySelector(link.dataset.goto)) {
-			const gotoBlock = document.querySelector(link.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+  function scrollingOnClick(e) {
+    const link = e.target;
+    // const headerHeight = document.querySelector('.header').offsetHeight;
 
-			// If the link is clicked while opening the burger menu.
-      // Write the script for the burger menu yourself.
-			if (
-				burger.classList.contains('_active') ||
-				nav.classList.contains('_active')
-			) {
-				nav.classList.remove('_active');
-				burger.classList.remove('_active');
-			}
+    if (link.dataset.scrollLink && document.querySelector(link.dataset.scrollLink)) {
+      const gotoBlock = document.querySelector(link.dataset.scrollLink);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset;
+      // If your header has a fixed position, use the under value
+      // const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - headerHeight;
 
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: 'smooth',
-			});
-			e.preventDefault();
-		}
-	}
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
 }
